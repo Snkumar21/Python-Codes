9b6d2bf30561c7123f6452b54260ed70de1b5996
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-01-13 12:22:18 +0530

    Add files via upload

---
+++ arithmetic_operator.py
@@ -0,0 +1,22 @@
+#Arithmetic Operators
+
+#User Defined
+num1 = int(input("Enter the first number: "))
+
+num2 = int(input("Enter the second number: "))
+
+
+#Printed Add, Sub, Mul, Div, Mod, Exponent, Floor Division
+print("Addition : ", num1 + num2)
+
+print("Subtraction: ", num1 - num2)
+
+print("Multiplication: ", num1 * num2)
+
+print("Division: ", num1 / num2)
+
+print("Modulus: ", num1 % num2)
+
+print("Exponent: ", num1 ** num2)
+
+print("Floor Division: ", num1 // num2)
