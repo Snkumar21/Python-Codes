2b35cca8124ac994ce98fbc0b86cafe434a38676
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-01-14 19:41:31 +0530

    Add files via upload

---
+++ assignment_operator.py
@@ -0,0 +1,28 @@
+#Assignment Operator
+
+print("Assignment Operators")
+a = 10
+print("Initial value of a:", a)
+
+a += 5  # Same as a = a + 5
+print("After a += 5, a =", a)
+
+a -= 3  # Same as a = a - 3
+print("After a -= 3, a =", a)
+
+a *= 2  # Same as a = a * 2
+print("After a *= 2, a =", a)
+
+a /= 2  # Same as a = a / 2
+print("After a /= 2, a =", a)
+
+a %= 3  # Same as a = a % 3
+print("After a %= 3, a =", a)
+
+a **= 2  # Same as a = a ** 2
+print("After a **= 2, a =", a)
+
+a //= 2  # Same as a = a // 2
+print("After a //= 2, a =", a)
+
+print()
