2379e3b3c6b05c0c9ddb896715854cabdb93c8fd
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-01-16 11:45:54 +0530

    Add files via upload

---
+++ comparison_operator.py
@@ -0,0 +1,19 @@
+#Comparison Operator
+
+#User Defined
+num1 = int(input("Enter the first number: "))
+
+num2 = int(input("Enter the second number: "))
+
+if num1 == num2:
+    print("Both numbers are equal")
+elif num1 > num2:
+    print("Number", num1, "is greater than", num2)
+elif num1 < num2:
+    print("Number", num1, "is smaller than", num2)
+elif num1 >= num2:
+    print("Number", num1, "is greater than or equal to", num2)
+elif num1 <= num2:
+    print("Number", num1, "is smaller than or equal to", num2)
+else:
+    print("Number is not equal to")
