2155de2661b6212c557fbaa3eab3282c7b6a7cea
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-01-17 11:42:12 +0530

    Add files via upload

---
+++ identity_operator.py
@@ -0,0 +1,19 @@
+# Identity Operators
+print("Identity Operators")
+x = [1, 2, 3]
+y = [1, 2, 3]
+z = x
+
+print("x =", x)
+print("y =", y)
+print("z =", z)
+
+# is operator
+print("Does x is z? ->", x is z)  # True, because z is a reference to x
+print("Does x is y? ->", x is y)  # False, different objects with the same values
+
+# is not operator
+print("Does x is not y? ->", x is not y)  # True, x and y are not the same object
+print("Does x is not z? ->", x is not z)  # False, x and z are the same object
+
+print()
---
+++ members_operator.py
@@ -0,0 +1,9 @@
+# Membership Operators
+print("Membership Operators")
+my_list = [1, 2, 3, 4, 5]
+element = 3
+
+print(f"Is {element} in my_list? {'Yes' if element in my_list else 'No'}")
+print(f"Is 10 not in my_list? {'Yes' if 10 not in my_list else 'No'}")
+
+print()
