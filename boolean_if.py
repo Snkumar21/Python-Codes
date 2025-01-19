c53880e99bb7cf6279809fc3991f5902093f8d25
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-01-19 13:35:22 +0530

    Add files via upload

---
+++ boolean_if.py
@@ -0,0 +1,13 @@
+setFlag = True
+
+if setFlag:
+    print("Hello Everyone!!")
+    print("Solve the assignment.")
+    
+setFlag = False
+
+if setFlag:
+    print("Good Afternoon Everyone!!")
+    print("Solve the assignment")
+
+print("Executed False")
