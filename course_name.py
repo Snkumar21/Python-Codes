543ce18160307fe98b9183c8e1f9b7484af231b1
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-01-23 20:41:09 +0530

    Add files via upload

---
+++ course_name.py
@@ -0,0 +1,9 @@
+# Create a list which consist of course name for mca sem-2. Index 5 substitute the course name with pre-placement training.
+
+course_name = ['Python', 'DSA', 'Profile Building Communication', 'Software Project Management', 'Problem Solving', 
+                'Advanced JavaScript', 'Introduction to Emerging Technologies','Coding Toolkit']
+
+if len(course_name) > 5:
+    course_name[5] = 'Pre-Placement Training'
+    
+print(course_name)
