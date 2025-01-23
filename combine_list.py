df4f3a6598e3e9ad51e1a18ef366d7dcba102b09
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-01-23 20:38:28 +0530

    Add files via upload

---
+++ combine_list.py
@@ -0,0 +1,8 @@
+#Create 2 list with 5 elements each combine both the list and display it.
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [6, 7, 8, 9, 10]
+
+# Combine both the list using zip() function
+combined_list = (list1 + list2)
+print(combined_list)
