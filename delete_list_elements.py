dce5972094aabb792fbbdefe89dddd5536736db6
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-01-26 14:39:39 +0530

    Add files via upload

---
+++ delete_list_elements.py
@@ -0,0 +1,13 @@
+list_ds = [5, 6, 7, 'access-the-elements', 4.55, 20, 50]
+
+del list_ds[5] #delete element at index 5
+print(list_ds) #output
+
+list_ds.remove('access-the-elements') #remove
+print(list_ds)
+
+poped = list_ds.pop(1) #pop
+print("Popped value:", poped, "Remaining elements:", list_ds) #output
+
+list_ds.clear() #empty the list
+print(list_ds) #output
