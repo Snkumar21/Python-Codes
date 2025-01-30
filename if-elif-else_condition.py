801fdfa61163c281a6aec2f660155172979adfb9
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-01-30 10:19:45 +0530

    Add files via upload

---
+++ if-elif-else_condition.py
@@ -0,0 +1,10 @@
+#if-elif-else Conditional Statement Code
+
+x = int(input("Enter a number: "))
+
+if x > 10:
+    print("The number", x, "is greater than 10")
+elif x < 10:
+    print("The number", x, "is smaller than 10")
+else:
+    print("The number", x, "is equal to 10")
