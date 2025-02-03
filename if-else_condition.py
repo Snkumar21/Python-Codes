fe21f7205ffb84e8fe7dd0ee46f4aa5292002a93
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-02-03 10:17:34 +0530

    Add files via upload

---
+++ if-else_condition.py
@@ -0,0 +1,8 @@
+#if-else Conditional Statement Code
+
+x = int(input("Enter a number: "))
+
+if x > 10:
+    print("The number", x, "is greater than 10")
+else:
+    print("The number", x, "is smaller than 10")
