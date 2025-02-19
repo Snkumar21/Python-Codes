dd58b467fcfc1ca1309535dae184b1a16960a83c
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-02-19 15:54:25 +0530

    Add files via upload

---
+++ nested-if_condition.py
@@ -0,0 +1,10 @@
+#nested-if Condition Statement code
+
+x = int(input("Enter a number: "))
+
+if x > 10:
+    if x < 20:
+        if x == 15:
+            print("The number", x, "is equal to 15")
+        else:
+            print("The number is greater than 15")
