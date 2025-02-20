db7141e5e561c514ab48385121b75c05b3bef798
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-02-20 11:54:04 +0530

    Update fibonacci_series.py

--- fibonacci_series.py
+++ fibonacci_series.py
@@ -1,5 +1,11 @@
+# This is Fibonacci Series using user defined value.
 import functools
 
+# Value Inputter
 num = int(input("Enter the number of terms: "))
+
+# The main logic of the program
 fib_series = [functools.reduce(lambda x, _: x + [x[-1] + x[-2]], range(num-2), [0, 1])][:num]
+
+# Display the result
 print("Fibonacci Series:", fib_series)
