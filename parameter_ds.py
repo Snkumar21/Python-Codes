50663aa2cdc4b63e116ce6c62c7b44af44bcdbf2
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-02-21 12:34:21 +0530

    Add files via upload

---
+++ parameter_ds.py
@@ -0,0 +1,21 @@
+def gm(name):#name is called as a parameter
+    print("Good Morning",name)
+
+gm("Nitish")#actual value ---argument
+gm("AAAA")
+
+#method passing arguments
+def add(n1,n2):
+    res=n1+n2
+    print("Addition of two numbers is",res)
+
+add(10,20)
+
+#use of return value
+
+def add1(n1,n2):
+    res1=n1+n2
+    return res1
+
+res1=add1(10,12)
+print("The addition of two numbers is:",res1)
