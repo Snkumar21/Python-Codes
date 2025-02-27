f850c3e21b40777e47fe52c33e6a370d4950c822
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-02-27 09:57:21 +0530

    Update armstrong_no.py

--- armstrong_no.py
+++ armstrong_no.py
@@ -1,7 +1,11 @@
+# Example of Armstrong Number using user-defined value.
+
+# Input
 num = int(input("Enter a number: "))
 order = len(str(num))
 armstrong_sum = sum(int(digit) ** order for digit in str(num))
 
+# Display
 if num == armstrong_sum:
     print(f"{num} is an Armstrong number.")
 else:
