36944ab8d2d03fd44e2ec30085aa15c38d49bad2
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-02-27 10:03:44 +0530

    Update hierarchical_inheritance.py

--- hierarchical_inheritance.py
+++ hierarchical_inheritance.py
@@ -1,16 +1,21 @@
-# Example of Hierarchical
+# Example of Hierarchical Inheritance using pre-defined value.
 
+# Parent Class
 class Parent:
     def parentfunc(self):
         print("This function belongs to parent")
 
+# Child1 Class
 class Child1(Parent):
     def childfunc1(self):
         print("This function belongs to child 1")
+
+# Child2 Class
 class Child2(Parent):
     def childfunc2(self):
         print("This function belongs to child 2")
 
+# Calling the classes
 child1ob = Child1()
 child1ob.parentfunc()
 child1ob.childfunc1()
