62f6824914b1b1dcc1be181fe40af8cb4224487c
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-02-27 10:21:02 +0530

    Update leftangle_trianglestar_program.py

--- leftangle_trianglestar_program.py
+++ leftangle_trianglestar_program.py
@@ -1,3 +1,8 @@
+# Example of pattern program using star.
+
+# Input
 n = int(input("Enter the number of rows: "))
+
+# Logic and Display
 for i in range(1, n + 1):
     print(" " * (n - i) + "*" * i)
