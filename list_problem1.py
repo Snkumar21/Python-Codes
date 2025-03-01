5528d1e13fb054f77ca9bba6fce6167a3ad1e024
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-03-01 19:15:54 +0530

    Update list_problem1.py

--- list_problem1.py
+++ list_problem1.py
@@ -1,6 +1,7 @@
+# Example of displaying a element from the list using split method.
+
+# The List
 list1 = ["abc",[2,3,("mohit","the avengers")],1,2,3]
 
+# The logic
 print(list1[1][2][1].split()[-1])
-
-#print_list = list1[1][2][1]
-#print(print_list.split()[-1])
