9741374bd8a52e4559e715e24b4b9cc3ead863b2
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-03-02 15:00:07 +0530

    Update odd_even.py

--- odd_even.py
+++ odd_even.py
@@ -1,5 +1,9 @@
+# Example of Odd Even using Moduls '%' operator in python.
+
+# Input from user
 num = int(input("Enter a number :"))
 
+# main logic of the code
 if num % 2 == 0:
     print(num, "is even")
 else:
