aeb51f72a8f8846d150591e7449bd253ecf9ce19
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-03-02 15:03:01 +0530

    Update odd_even_using_and_operator.py

--- odd_even_using_and_operator.py
+++ odd_even_using_and_operator.py
@@ -1,6 +1,10 @@
+# Example of Odd Even using AND Operator in python.
+
+# Input from the user
 num = int(input("Enter a number: "))
 
-if (num & 1) == 0: #Tu isme % use nahi kar sakta
+# Main logic of the code.
+if (num & 1) == 0:
     print("The number is even")
 else:
     print("The number is odd")
