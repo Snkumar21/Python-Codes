f385534033b857cd766e608f458942a693a8abdc
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-03-03 22:28:35 +0530

    Add files via upload

---
+++ palindrome_user_fun.py
@@ -0,0 +1,20 @@
+# This is Palindrome finder using user-defined function
+class PalindromeChecker:
+    def __init__(self, num):
+        self.num = num
+
+    # main logic of the program
+    def is_palindrome(self):
+        return str(self.num) == str(self.num)[::-1]
+
+# Input number
+num = int(input("Enter a no.: "))
+
+# Function calling
+checker_obj = PalindromeChecker(num)
+
+# Display the output
+if checker_obj.is_palindrome():
+    print(f"{num} is a Palindrome number.")
+else:
+    print(f"{num} is NOT a Palindrome number.")
