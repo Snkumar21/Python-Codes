ec991541a1a22e7e4064b209c43fcc1e7ad1fb75
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-03-03 22:30:47 +0530

    Delete leftangle_trianglestar_program.py

--- leftangle_trianglestar_program.py
+++
@@ -1,8 +0,0 @@
-# Example of pattern program using star.
-
-# Input
-n = int(input("Enter the number of rows: "))
-
-# Logic and Display
-for i in range(1, n + 1):
-    print(" " * (n - i) + "*" * i)
