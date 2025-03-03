c0ff47a8e41e905e067df9ae3a876cc82b4f0934
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-03-03 22:36:00 +0530

    Add files via upload

---
+++ pattern_floyd_triangle.py
@@ -0,0 +1,7 @@
+# Input from the user
+n = int(input("Enter the number of rows: "))
+num = 1
+
+# Main logic of the program
+for i in range(1, n + 1):
+    print(' ' * (n - i) + ' '.join(str(num) for num in range(1, i + 1)))
