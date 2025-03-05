9e0c915b9e5b91f2e4b65b8fcbffe246f8b540db
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-03-05 09:41:50 +0530

    Add files via upload

---
+++ pattern_square.py
@@ -0,0 +1,12 @@
+# Input from the user
+n = int(input("Enter the number of rows: "))
+num = 1
+
+# Main logic of the program
+for i in range(n):
+    for j in range(n):
+        if i == 0 or i == n - 1 or j == 0 or j == n - 1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
