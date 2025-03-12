c09bcc2ecf045aa9ef5a8d3a3287aa3b1e261757
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-03-12 16:31:09 +0530

    Add files via upload

---
+++ replace_list.py
@@ -0,0 +1,11 @@
+#Consider the list with following elements: -
+#ls = 20, 30, 40, 20, 50
+#Find and replace the first occurrence of 20 with 10.
+
+ls = [20, 30, 40, 20, 50]
+
+if 20 in ls:
+    index = ls.index(20)
+    ls[index] = 10
+    
+print(ls)
