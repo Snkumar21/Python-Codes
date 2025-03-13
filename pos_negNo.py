160c1d57264a262cb72d9fb5312fa4aa28844d1f
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-03-13 10:37:02 +0530

    Add files via upload

---
+++ pos_negNo.py
@@ -0,0 +1,7 @@
+# Program to input a number from the use and display wreathe the number is positive or not
+
+num = int(input("Enter a number: "))
+if num > 0:
+    print("Positive number")
+if num < 0:
+    print("Negative number")
