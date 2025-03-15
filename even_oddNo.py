4852c71c39b7dfb513580fd93c26911052b40b25
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-03-15 19:46:48 +0530

    Add files via upload

---
+++ even_oddNo.py
@@ -0,0 +1,7 @@
+# Program to input a number from the use and display wreathe the number is even or odd
+
+no = int(input("Enter a number: "))
+if no % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
