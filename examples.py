3059d32c8619192e8de87d0c509c118660db4d81
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-03-15 19:48:12 +0530

    Add files via upload

---
+++ examples.py
@@ -0,0 +1,13 @@
+no = 3
+if no < 5:
+    print("The number is less than 5")
+
+setflag = True
+if setflag:
+    print("Good Morning!")
+    print("Solve the assignment")
+setflag = False
+if setflag:
+    print("Good Afternoon!")
+    print("Solve the assignment")   
+print("Execute false") # Irrespective of the condition, this statement will be executed
