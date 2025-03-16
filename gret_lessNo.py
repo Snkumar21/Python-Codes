3fdbafdf2e92d36f99875cad3ae2ccddb966809e
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-03-16 20:12:32 +0530

    Add files via upload

---
+++ gret_lessNo.py
@@ -0,0 +1,10 @@
+# Program to input a number from the use and display wreathe the number is greater or less
+
+no1 = int(input("Enter first number: "))
+no2 = int(input("Enter second number: "))
+if no1 > no2:
+    print("The first number is greater than the second number")
+elif no1 < no2:
+    print("The first number is less than the second number")
+else:
+    print("The first number is equal to the second number")
