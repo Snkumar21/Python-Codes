88b8071a670871bcec5eeef5da9d013f44f86ae3
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-03-17 19:01:03 +0530

    Add files via upload

---
+++ reverse_user_fun.py
@@ -0,0 +1,13 @@
+class NumberReverser:
+    def __init__(self, num):
+        self.num = num
+
+    def reverse_number(self):
+        rev_num = int(str(self.num)[::-1])
+        return rev_num
+
+num = int(input("Enter a number: "))
+
+reverser_obj = NumberReverser(num)
+
+print(f"The reverse of {num} is: {reverser_obj.reverse_number()}")
