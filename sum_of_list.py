3f61bf1d67fe6070a72c6d49dde8b3d77ff5cbb1
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-03-19 17:26:39 +0530

    Add files via upload

---
+++ sum_of_list.py
@@ -0,0 +1,7 @@
+#Create a list of 10 numbers and display the sum of the number.
+
+ls = [20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+total_sum = sum(ls)
+
+print("The sum of the numbers in the list is: ", total_sum)
