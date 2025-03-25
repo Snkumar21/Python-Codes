772268939abb89ec3b8f1fa9c4e782840b42157c
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-03-25 11:05:09 +0530

    Update class_twoMethods.py

--- class_twoMethods.py
+++ class_twoMethods.py
@@ -1,4 +1,5 @@
 # Write a program to implement a class which has at least two methods i.e. to input the string from the user and display it in upper case?
+# Example of displaying a string in a upper case using a class operator.
 
 class String:
     def __init__(self):
