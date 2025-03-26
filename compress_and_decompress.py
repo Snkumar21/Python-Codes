07d591270dec580768f1ff7ee499868f74d535d7
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-03-26 10:44:31 +0530

    Update compress_and_decompress.py

--- compress_and_decompress.py
+++ compress_and_decompress.py
@@ -1,5 +1,9 @@
+# Example of compress and decompress in python.
+
 import zlib
+
 data = input("Enter a string: ").encode()
 compressed = zlib.compress(data)
+
 print("Compressed:", compressed)
 print("Decompressed:", zlib.decompress(compressed).decode())
