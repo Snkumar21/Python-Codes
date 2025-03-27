ba8f2d50a29bb6ff0fe29e05f3b6dff9e88b1698
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-03-27 09:34:30 +0530

    Update even_list.py

--- even_list.py
+++ even_list.py
@@ -1,4 +1,5 @@
-# Generate a list of even numbers between 1 and 100
+# Example of generating a list of even numbers between 1 and 100 in python.
+
 even_numbers = [num for num in range(1, 100) if num % 2 == 0]
 
 # Display the list of even numbers
