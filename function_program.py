8110d662d436e0148f214955d180eb584cc92c6b
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-03-27 09:53:17 +0530

    Update function_program.py

--- function_program.py
+++ function_program.py
@@ -1,17 +1,19 @@
-def gm(name):#name is called as a parameter
+# Example of functions in python...
+
+def gm(name): # name is called as a parameter
     print("Good Morning",name)
 
-gm("Devyani")#actual value ---argument
+gm("Nitish") # actual value ---argument
 gm("AAAA")
 
-#method passing arguments
+# method passing arguments
 def add(n1,n2):
     res=n1+n2
     print("Addition of two numbers is",res)
 
 add(10,20)
 
-#use of return value
+# use of return value
 
 def add1(n1,n2):
     res1=n1+n2
@@ -20,12 +22,12 @@ def add1(n1,n2):
 res1=add1(10,12)
 print("The addition of two numbers is:",res1)
 
-#Use of pass statement inside function
+# Use of pass statement inside function
 def greet():
     pass
 print("pass inside a function")
 
-#function with default arguments
+# function with default arguments
 def add2(n1=10,n2=33):  
     res2=n1+n2
     print("The addition of two numbers with default values:",res2)
@@ -37,7 +39,7 @@ add2(27)
 # function call with no arguments
 add2()
 
-#use of keyword arguments
+# use of keyword arguments
 def display(fName, lName):
     print('First Name:', fName)
     print('Last Name:', lName)
