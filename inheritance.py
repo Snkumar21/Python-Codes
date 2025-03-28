17911adaf1c179d08bc44b00d5f4a0de2922ded5
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-03-28 10:03:38 +0530

    Update inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -1,10 +1,13 @@
+# Example of inheritance in python...
 
 class Parent:
     def parentFunc(self):
         print("Parent Function")
+
 class Child(Parent):
     def childFunc(self):
         print("Child Function")
+
 ch = Child()    
 ch.parentFunc()
 ch.childFunc()
