94f93c4ede7b22f9ef1fc26e54d1f6ff6d9db22b
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-03-31 11:17:40 +0530

    Update pos_negNo.py

--- pos_negNo.py
+++ pos_negNo.py
@@ -1,4 +1,4 @@
-# Program to input a number from the use and display wreathe the number is positive or not
+# Example of input a number from the use and display wreathe the number is positive or not in python...
 
 num = int(input("Enter a number: "))
 if num > 0:
