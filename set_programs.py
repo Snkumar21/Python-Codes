09a56c3d6917f716d505de9b6a67d3471b090b19
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-04-02 11:55:16 +0530

    Update set_programs.py

--- set_programs.py
+++ set_programs.py
@@ -1,10 +1,12 @@
-# Set
+# Example of set programs in python...
+
 set_ds = {1,1,2,3,3,4,5,6,7,7} 
 print(set_ds)
 set_ds.add(55)
 print(set_ds)
 set_ds.remove(55)
 print(set_ds)
+
 #Operations
 set_ds1 = {10,20,30,40,50}
 set_ds2 = {10,20,100,200,300}
