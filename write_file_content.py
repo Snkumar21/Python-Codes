3dcad9f5d2602351c0d80e3831293ae997b34590
Nitish <snkumar6926@gmail.com>
2025-04-04 20:45:00 +0530

    Final conflict resolution

--- write_file_content.py
+++ write_file_content.py
@@ -1,8 +1,15 @@
+# Example of write a file content in python...
+
+# Open the file in write mode
 file2 = open("demotextfile.txt", "w")
 
+# Write the content to the file
 file2.write("Hello, this is nitish...\n")
 file2.write("Welcome to the world")
 
+# Re-opening the file in read mode to read the content
 file2 = open("demotextfile.txt")
 readfile = file2.read()
+
+# Print the content of the file
 print(readfile)
