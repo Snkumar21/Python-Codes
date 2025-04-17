0a1fa55ed0ffcac1e2a1664a7d54d73e37e8619a
Nitish <snkumar6926@gmail.com>
2025-04-17 18:14:29 +0530

    new changes added

---
+++ count_and_display_total_number.py
@@ -0,0 +1,13 @@
+def count_total_words(filename):
+    total_words = 0
+    try:
+        with open(filename, 'r') as file:
+            for line in file:
+                words = line.split()
+                total_words += len(words)
+        print(f"Total number of words: {total_words}")
+    except FileNotFoundError:
+        print("File not found.")
+
+# Example usage:
+count_total_words("demotextfile.txt")
