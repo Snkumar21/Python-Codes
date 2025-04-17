50b40bfc23730e474168fcfe3afaf5f8169545ba
Nitish <snkumar6926@gmail.com>
2025-04-17 18:12:25 +0530

    new changes added

---
+++ count_number_of_lines.py
@@ -0,0 +1,13 @@
+def count_lines_not_starting_with_T(filename):
+    count = 0
+    try:
+        with open(filename, 'r') as file:
+            for line in file:
+                if not line.strip().startswith('T'):
+                    count += 1
+        print(f"Lines not starting with 'T': {count}")
+    except FileNotFoundError:
+        print("File not found.")
+
+# Example usage:
+count_lines_not_starting_with_T("demotextfile.txt")
