df66ebd68c9ff06cb64ad677904b76aa741d3b0c
Nitish <snkumar6926@gmail.com>
2025-06-04 11:15:59 +0530

    Updates added

--- pattern_number_traingle.py
+++ pattern_number_traingle.py
@@ -4,11 +4,3 @@ n = int(input("Enter the number of rows: "))
 # Main logic of the program
 for i in range(1, n + 1):
     print(' ' * (n - i) + ' '.join(str(num) for num in range(1, i + 1)))
-
-# Example of printing pattern using numbers.
-# Input from the user
-n = int(input("Enter the number of rows: "))
-
-# Main logic of the program
-for i in range(1, n + 1):
-    print(' ' * (n - i) + ' '.join(str(num) for num in range(1, i + 1)))
