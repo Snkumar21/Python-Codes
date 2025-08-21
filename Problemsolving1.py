343bfd75d7b8e09e2e3712daddaf91ec82a9cbca
Nitish <snkumar6926@gmail.com>
2025-08-21 21:46:19 +0530

    new changes added

---
+++ Problemsolving1.py
@@ -0,0 +1,15 @@
+def find_village_name(villagers):
+    # Take the first name as base
+    prefix = villagers[0]
+
+    # Check prefix with all other names
+    for name in villagers[1:]:
+        while not name.startswith(prefix):
+            prefix = prefix[:-1]  # reduce prefix
+            if prefix == "":
+                return ""  # no common village name
+    return prefix
+
+# Example
+villagers = ["ramkrishan", "ramsudah", "ramlakhan", "ramesh"]
+print("Village Name:", find_village_name(villagers))
