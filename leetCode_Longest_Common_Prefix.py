a2b73418272d4716b66adf414d55051266f58317
Nitish <snkumar6926@gmail.com>
2025-09-15 20:55:23 +0530

    New Code

---
+++ leetCode_Longest_Common_Prefix.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+
+        # Start with the first string as prefix
+        prefix = strs[0]
+
+        for word in strs[1:]:
+            # Shrink prefix until it matches the beginning of word
+            while word.find(prefix) != 0:
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        return prefix
